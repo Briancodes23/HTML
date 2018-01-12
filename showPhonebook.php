b3930f04810d07a12454ca5037c64f769452cdc7
Brian Oshinaike <brianoshinaike23@gmail.com>
2018-01-12 17:16:06 +0000

    Update showPhonebook.php

--- showPhonebook.php
+++ showPhonebook.php
@@ -1,7 +1,8 @@
+<!--The purpose of this HTML/PHP code is to enable contact data to recognized & found by file:StaffPhonebook.html -->
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-gb">
 <head>
 <title>Staff Phonebook</title>
-<meta name="Author" content="mk05@gre.ac.uk" />
+<meta name="Author" content="emailAddress@gmail.com" />
 <meta name="Description" content="Shows all records in staff phonebook" />
 <style type="text/css">
 body { padding:15px; font-family:sans-serif; }
@@ -21,29 +22,30 @@ Return to the <a href="Phonebook.html">Staff Phonebook Homepage</a>
 </tr>
 <?php
 
-// Set up the connection
+//Seting up the connection
 if (!$conn = new COM('ADODB.Connection')) exit('Unable to create an ADODB connection');
 
 //Define database string, specify database drive 
-$strConn = 'DRIVER={Microsoft Access Driver (*.mdb)} DBQ=E \WWW.\LESOCO\33085962\DB1\Phonebook.mdb';
+$strConn = 'DRIVER={Microsoft Access Driver (*.mdb)} DBQ=E \WWW.\GRE\33085962\DB1\Phonebook.mdb';
 
 //Open connection to the database
 $conn->open($strConn);
 
-//SQL statment 
+//SQL statment to retieve data 
 $strSQL = 'SELECT id, firstname, surname, phone FROM staff ORDER BY id';
 
 //Execute SQL statment and return records
 $rs = $conn->execute($strSQL);
 
-// What is Fields(0)?? Fields(1) is firstname, Fields(2) is surname and Fields(3) is phone
-//Carry on looping while there are records, exit when end of file 
+
+//While loop used to carrry on looping while there are records, exits at end of the file
 while (!$rs->EOF) {
   echo '<tr><td>' . $rs->Fields(0)->value . '</td>';
   echo '<td>' . $rs->Fields(1)->value . '</td>';
   echo '<td>' . $rs->Fields(2)->value . '</td>';
   echo '<td>' . $rs->Fields(3)->value . '</td></tr>';
-  //Move on to next record 
+  
+  //Moving on to next record 
   $rs->MoveNext(); 
 }
 ?>
